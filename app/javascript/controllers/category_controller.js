49a09cd56789ce3f322d41c4321dccdede78df48
luca gastaldi <lucagastaldi82@gmail.com>
2024-04-18 11:28:55 +0100

    Fixed test

--- app/javascript/controllers/category_controller.js
+++ app/javascript/controllers/category_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js"
 
 export default class extends Controller {
-  static targets = ["subcategory", "services"]
+  static targets = ["category", "subcategory"]
   static values = {
     url: String
   }
 
-  subcategory(event) {
-    let category = event.target.value
+  connect() {
+    let category = this.categoryTarget.value
     let target = this.subcategoryTarget.id
     let url = this.urlValue
 
@@ -17,11 +17,14 @@ export default class extends Controller {
     })
   }
 
-  subcategory_select() {
-    console.log("triggered!")
+  subcategory(event) {
+    console.log("category: ", )
+    let category = event.target.value
+    let target = this.subcategoryTarget.id
+    let url = this.urlValue
 
-    let subcategory = this.subcategoryTarget.value
-    this.element.dispatchEvent(new CustomEvent("subcategory-select", { detail: { subcategory } }))
-
+    get(`${url}?target=${target}&category=${category}`, {
+      responseKind: "turbo-stream"
+    })
   }
 }
