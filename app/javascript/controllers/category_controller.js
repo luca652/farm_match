c3b18d4316bd0827b19e781684e096d52bef51b4
luca gastaldi <lucagastaldi82@gmail.com>
2024-06-06 12:28:40 +0100

    Fix issue with & character in turbo stream request Add encodeURIcomponent to escape special characters, fixing issue with & Remove unnecessary code from connect method that caused error on page load

--- app/javascript/controllers/category_controller.js
+++ app/javascript/controllers/category_controller.js
@@ -7,18 +7,8 @@ export default class extends Controller {
     url: String
   }
 
-  connect() {
-    let category = this.categoryTarget.value
-    let target = this.subcategoryTarget.id
-    let url = this.urlValue
-
-    get(`${url}?target=${target}&category=${category}`, {
-      responseKind: "turbo-stream"
-    })
-  }
-
   subcategory(event) {
-    let category = event.target.value
+    let category = encodeURIComponent(event.target.value)
     let target = this.subcategoryTarget.id
     let url = this.urlValue
 
