ccb7e319b864eaa06b86ae59c344e279a5e4a9b2
luca gastaldi <lucagastaldi82@gmail.com>
2024-06-06 14:28:28 +0100

    Add model tests for answer Test attributes and association with service

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let(:user) { User.create(name: 'Marco')}
+  # let(:job) { Job.create(headline: 'First Job',
+  #                        description: 'Decent Job',
+  #                        user_id: user.id,
+  #                        category: 'Agri Contracting',
+  #                        subcategory: 'Application (Spraying and Spreading)',
+  #                        services_attributes: [{ name: 'Fertilizer Spreading' }])
+  #                       }
+  # let(:service) { job.services.first }
+  # let(:answer) { Answer.create(answer: "Yes", label: "Test Label", service_id: service.id)}
+
+  # it 'has an answer' do
+  #   expect(answer.answer).to eq("Yes")
+  # end
+
+  # it 'has a label' do
+  #   expect(answer.label).to eq("Test Label")
+  # end
+
+  # describe "Associations" do
+  #   it 'belongs to a service' do
+  #     expect(answer.service).to eq(service)
+  #   end
+  # end
 end
