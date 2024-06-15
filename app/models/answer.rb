606a4dd60711292da1fbf968095f8e4327a1c798
luca gastaldi <lucagastaldi82@gmail.com>
2024-06-15 07:23:30 +0100

    AREA_QUESTION and DESCRIPTION_QUESTION in Answer Repeated questions can be stored separately and referenced in QUESTIONS hash

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -19,13 +19,19 @@ class Answer < ApplicationRecord
                description: 9
   }.freeze
 
+  AREA_QUESTION = { kind: :area, wording: "Estimated number of acres/hectacres?", options: ["acres", "hectares"], label: "Number of acres/hectacres"}
+  DESCRIPTION_QUESTION = { kind: :description, wording: "Please write a short decription of the job", label: "Description"}
+
   QUESTIONS = { 'Fertilizer Spreading' => [{ kind: :multiple_choice, wording: "What type of fertilizer do you want spread?", label: "Type of fertilizer", options: ["granular", "liquid"]},
-                                           { kind: :area, wording: "Estimated number of acres/hectacres?", label: "Number of acres/hectacres", options: ["acres", "hectares"]},
-                                           { kind: :description, wording: "Please write a short decription of the job", label: "description"}],
-                'Lime Spreading' => [],
-                'Spraying (specialised)' => [],
-                'Spraying (standard)' => []
-              }
+                                           AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Lime Spreading' => [{ kind: :multiple_choice, wording: "Do you need a loader?", options: ["yes", "no"], label: "Loader"},
+                                      AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Spraying (specialised)' => [{ kind: :multiple_choice, wording: "What type of spraying do you require?", options: ["ATV spraying", "Avadex application", "Slug pelleting", "Weed wiping", "Other"]},
+                                             AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Spraying (standard)' => [{ kind: :multiple_choice, wording: "What type of spraying do you require?", options: ["Arable spraying", "Grassland spraying"]},
+                                          AREA_QUESTION, DESCRIPTION_QUESTION]
+                                            }
+
 
   def convert_and_store_area
     if details["unit"] == "acres"
