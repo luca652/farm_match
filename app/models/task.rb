c896cac2c7bbde33ed403c36ad590e174bd68c56
luca gastaldi <lucagastaldi82@gmail.com>
2024-07-11 15:43:49 +0100

    Location validation error - Add custom validation location_is_present in Task model. If either longitude or latitude is not set, we get the custom error "Location can't be blank" - Why? Previously errors were 'latitude/longitude can't be blank', but user is not aware of those fields.

--- app/models/task.rb
+++ app/models/task.rb
@@ -33,12 +33,14 @@ class Task < ApplicationRecord
   has_many :services, dependent: :destroy
   accepts_nested_attributes_for :services, allow_destroy: true
 
-  validates :category, :subcategory, :headline, :description, :latitude, :longitude, presence: true
+  validates :category, :subcategory, :headline, :description, presence: true
   validates :category, inclusion: { in: CATEGORIES }
   validates :subcategory, inclusion: { in: SUBCATEGORIES.values.flatten }
   # inclusion expects an array, but SUBCATEGORIES.values returns an array of arrays --> flatten solves the issue
+
   validates :services, presence: true
   validate :valid_subcategory_for_category
+  validate :location_is_present
 
 
   def valid_subcategory_for_category
@@ -47,6 +49,12 @@ class Task < ApplicationRecord
     valid_subcategories = SUBCATEGORIES[category]
     unless valid_subcategories.include?(subcategory)
       errors.add(:subcategory, "is not valid for the selected category")
+    end
+  end
+
+  def location_is_present
+    if latitude.blank? || longitude.blank?
+      errors.add(:base, "Location can't be blank")
     end
   end
 end
