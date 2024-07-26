ac77ebb441a34fa0949e4bf01b4d4b08bf204bb5
luca gastaldi <lucagastaldi82@gmail.com>
2024-07-26 16:06:09 +0100

    Refactor new and edit forms into partials New and edit views now render two partials: _form_step_one and _form_step_two Added helper methods in controllers/helpers/tasks_helpers.rb that set the path for the forms, the urls for the back button in step two, and the text on the submit button.

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  def url_for_step_one
+    @task.new_record? ? create_step_one_tasks_path : update_step_one_task_path(@task)
+  end
+
+  def url_for_back_button
+    @task.new_record? ? new_step_one_tasks_path : edit_step_one_task_path
+  end
+
+  def submit_text
+    @task.new_record? ? 'Create Task' : 'Update Task'
+  end
 end
