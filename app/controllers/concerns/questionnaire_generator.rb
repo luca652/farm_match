f8fb5850e096bc9f4ce5ac363913b02d4ea0b927
luca gastaldi <lucagastaldi82@gmail.com>
2024-07-30 15:24:28 +0100

    Create Questionnaire Generator Add questionnaire_generator.rb to concerns. Move here the QUESTIONS hash and the logic for generating questions.

---
+++ app/controllers/concerns/questionnaire_generator.rb
@@ -0,0 +1,37 @@
+module QuestionnaireGenerator
+  extend ActiveSupport::Concern
+
+  AREA_QUESTION = { kind: :area, wording: "Estimated number of acres/hectacres?", options: ["acres", "hectares"], label: "Area"}
+  DESCRIPTION_QUESTION = { kind: :description, wording: "Please write a short decription of the task", label: "Description"}
+
+  QUESTIONS = { 'Fertilizer Spreading' => [{ kind: :multiple_choice, wording: "What type of fertilizer do you want spread?", label: "Type of fertilizer", options: ["granular", "liquid"]},
+                                           AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Lime Spreading' => [{ kind: :multiple_choice, wording: "Do you need a loader?", options: ["yes", "no"], label: "Loader"},
+                                      AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Spraying (specialised)' => [{ kind: :multiple_choice, wording: "What type of spraying do you require?", label: "Type of spraying", options: ["ATV spraying", "Avadex application", "Slug pelleting", "Weed wiping", "Other"]},
+                                             AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Spraying (standard)' => [{ kind: :multiple_choice, wording: "What type of spraying do you require?", label: "Type of spraying", options: ["Arable spraying", "Grassland spraying"]},
+                                          AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Cereals - Drilling' => [{ kind: :multiple_choice, wording: "Preferred method of drilling?", label: "Drilling method", options: ["Any method", "Combi-drill", "Conventional", "Direct"]},
+                                         AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Grass Seed - Drilling' => [{ kind: :multiple_choice, wording: "Preferred method of drilling?", label: "Drilling method", options: ["Any method", "Broadcast", "Cross Drilling", "Harrow"]},
+                                            AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Maize - Drilling' => [{ kind: :multiple_choice, wording: "Preferred method of drilling?", label: "Drilling method", options: ["Precision", "Under film"]},
+                                       AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Oil Seed Rape - Drilling' => [{ kind: :multiple_choice, wording: "Preferred method of drilling?", label: "Drilling method", options: [] },
+                                               AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Potatoes - Drilling' => [AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Sugar Beet - Drilling' => [AREA_QUESTION, DESCRIPTION_QUESTION],
+                'Vegetables - Drilling' => [{ kind: :multiple_choice_with_other, wording: "What type of vegetables are you sowing?", label: "Type of vegetable", options: [ "Broccoli", "Cabbage", "Carrots", "Cauliflower", "Kale", "Leek", "Lettuce", "Onion", "Peas", "Rhubarb", "Turnip", "Other"]},
+                                            AREA_QUESTION, DESCRIPTION_QUESTION ],
+                'Fence Erection' => [{ kind: :multiple_choice, wording: "What type of fence do you require?", label: "Type of fence", collection: ["Post and four barb", "Post and three rails", "Post, stock, net and two barb"]},
+                                     { kind: :short_length, wording: "Estimated length of mowing required? (Pick one measurement)", label: "Estimated length", collection: ["Feet", "Meters", "Miles", "Kilometers"]}]
+
+                }
+
+  def generate_questions_for_service(service)
+    QUESTIONS[service.name].map do |question_attrs|
+      service.questions.build(question_attrs)
+    end
+  end
+end
